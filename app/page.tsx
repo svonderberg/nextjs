72c5d4d3c39f65d1c49a20db4f8a96171b85c861
Scott Vonderberg <svonderberg@MacBook-Air.local>
2026-01-05 14:00:00 -0600

    work

--- app/page.tsx
+++ app/page.tsx
@@ -27,6 +27,7 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
         </div>
+        blah blah blah
       </div>
     </main>
   );
